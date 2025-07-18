c9fd2d11f9cec8581d21ff4a71e7d309f79102bc
Márcia Ellen <ellenmarica6@gmail.com>
2025-07-18 14:53:40 +0000

    introduçao_do_projeto

---
+++ projeto.py
@@ -0,0 +1,58 @@
+
+c_abacaxi={'Arren': 5.0, 
+           'mudas': 100, 
+           'insumos': 30, 
+           'maoobra': 60,
+           'energ': 600}
+
+b_abacaxi= {'valor':3.0,
+            'kg/hec': 500}
+
+c_morango={'Arren': 6.0, 
+           'mudas': 200, 
+           'insumos': 40, 
+           'maoobra': 90,
+           'energ': 600}
+
+b_morango= {'valor':7.0,
+            'kg/hec': 600}
+
+c_laranja={'Arren': 5.0, 
+           'mudas': 100, 
+           'insumos': 30, 
+           'maoobra': 60,
+           'energ': 600}
+
+b_laranja= {'valor':3.0,
+            'kg/hec': 500}
+
+c_banana={'Arren': 5.0, 
+           'mudas': 150, 
+           'insumos': 50, 
+           'maoobra': 60,
+           'energ': 500}
+
+b_banana= {'valor':2.0,
+            'kg/hec': 540}
+
+c_tomate={'Arren': 5.0, 
+           'mudas': 50, 
+           'insumos': 20, 
+           'maoobra': 60,
+           'energ': 300}
+
+b_tomate= {'valor':3.0,
+            'kg/hec': 300}
+
+
+quanthec = int(input("Digite a quantidade de hectáres: "))
+produto = input("Informe o produto: ")
+
+abacaxi = 1 
+morango = 2 
+laranja = 3
+banana = 4
+tomate = 5 
+ 
+
+
